7a888d7a8ed05a5df4e1c43afe75b20bb37f8fbb
zevgen83 <108744200+zevgen83@users.noreply.github.com>
2022-11-18 01:09:45 +0200

    js-hw-08.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const localKey = "videoplayer-current-time";
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+function onPlay(evt) {
+    localStorage.setItem(localKey, evt.seconds);      
+};
+   
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentPlaybackPosition = localStorage.getItem(localKey);
+
+player.setCurrentTime(currentPlaybackPosition);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+let valueFromStorage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+console.log('valueFromStorage:', valueFromStorage);
+
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+onSaveData();
+
+refs.form.addEventListener('input', throttle(onTextForm, 500));
+refs.form.addEventListener("submit", handleSubmit);
+
+function onTextForm(evt) { 
+    const nameInput = evt.target.name;
+    const textInput = evt.target.value;
+    valueFromStorage[nameInput] = textInput;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(valueFromStorage));
+}
+
+function handleSubmit(evt) {  
+  if (!valueFromStorage.email && !valueFromStorage.message) {
+    return alert("All fields must be filled!!!!");
+  }
+  evt.preventDefault();
+  console.log(valueFromStorage);
+  valueFromStorage = {};
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);     
+}
+      
+
+function onSaveData() {
+  if (valueFromStorage) {
+    refs.email.value = valueFromStorage.email || '';
+    refs.message.value = valueFromStorage.message || '';
+  }
+} 
